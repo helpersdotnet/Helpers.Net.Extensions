47701a8631008d4633ceb05ed218d6748ecf3ad0
Prabir Shrestha <mail@prabir.me>
2009-12-26 15:50:06 +0700

    + bool IsEven(this int i)

---
+++ src/Helpers.Net.Extensions/int.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Helpers.Net.Extensions
+{
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// Checks whether the number is even or not.
+        /// </summary>
+        /// <param name="i">The number to check.</param>
+        /// <returns>Returns true if the number is even.</returns>
+        public static bool IsEven(this int i)
+        {
+            return i % 2 == 0;
+        }
+    }
+}
