f2544c88ca3e10d2d7dc67f4107d066fb652b6cb
Prabir Shrestha <mail@prabir.me>
2009-12-26 15:26:09 +0700

    decimal ToDecimal(this string s)

---
+++ src/Helpers.Net.Extensions/string.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Helpers.Net.Extensions
+{
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// Converts the string representation of a number to <see cref="System.Decimal"/> equivalent.
+        /// </summary>
+        /// <param name="s">String representation of the number to convert.</param>
+        /// <returns></returns>
+        public static decimal ToDecimal(this string s)
+        {
+            return decimal.Parse(s);
+        }
+    }
+}
---
+++ src/TestProject/StringConversionTest.cs
@@ -0,0 +1,79 @@
+﻿using Helpers.Net.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestProject
+{
+
+
+    /// <summary>
+    ///This is a test class for StringConversionTest and is intended
+    ///to contain all StringConversionTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class StringConversionTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for ToDecimal
+        ///</summary>
+        [TestMethod()]
+        public void ToDecimalTest()
+        {
+            string s = "1.0"; // TODO: Initialize to an appropriate value
+            decimal expected = 1.0M;
+            decimal actual = s.ToDecimal();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
