9c585b3313e65be42b72457631ef9f87373a5906
Prabir Shrestha <mail@prabir.me>
2009-12-26 12:56:05 +0700

    + string ToHexString(this Color color)

---
+++ src/Helpers.Net.Extensions/Color/Color.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Drawing;
+
+namespace Helpers.Net.Extensions
+{
+    public static class Extensions
+    {
+        /// <summary>
+        /// Converts a color object into a hex color value
+        /// that can be used as an HTML color
+        /// </summary>
+        /// <param name="color">System.Drawing.Color</param>
+        /// <returns>a hex value</returns>
+        public static string ToHexString(this Color color)
+        {
+            string HexR = null;
+            string HexB = null;
+            string HexG = null;
+
+            try
+            {
+                //Get Red Hex
+                HexR = Convert.ToString(color.R, 16).ToUpper();
+                if (HexR.Length < 2)
+                    HexR = "0" + HexR;
+
+                //Get Green Hex
+                HexG = Convert.ToString(color.G, 16).ToUpper();
+                if (HexG.Length < 2)
+                    HexG = "0" + HexG;
+
+                //Get Blue Hex
+                HexB = Convert.ToString(color.B, 16).ToUpper();
+                if (HexB.Length < 2)
+                    HexB = "0" + HexB;
+
+            }
+            catch
+            {
+                return "";
+            }
+
+            return "#" + HexR + HexG + HexB;
+        }
+    }
+}
