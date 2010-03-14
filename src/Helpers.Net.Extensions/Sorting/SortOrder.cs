ee72b24fae8f4767e4d09479d8e23e6b876ee104
Prabir Shrestha <mail@prabir.me>
2010-03-14 13:01:00 +0700

    + sort order enum

---
+++ src/Helpers.Net.Extensions/Sorting/SortOrder.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Helpers.Net.Extensions.Sorting
+{
+    public enum SortOrder
+    {
+        /// <summary>
+        /// Sort in ascending order.
+        /// </summary>
+        Ascending = 0,
+
+        /// <summary>
+        /// Sort in descending order.
+        /// </summary>
+        Descending = 1
+
+    }
+}
